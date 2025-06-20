ff59deca3cfe3c5af3d620be8f8cd1063715bb54
prates <silva.prates@aluno.ifsp.edu.br>
2025-06-20 16:21:16 -0300

    0.002

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+const body = document.querySelector("body");
+
+//menu lateral
+const menuLateral = document.getElementById("menuLateral");
+const home = document.createElement("button")
+const comandos = document.createElement("button")
+const lista = document.createElement("ul")
+const item1 = document.createElement("li")
+const item2 = document.createElement("li")
+home.innerText = "Home";
+comandos.innerText = "Comandos";
+menuLateral.appendChild(lista);
+lista.append(item1, item2);
+item1.appendChild(home);
+item2.appendChild(comandos);
+
+//caminho principal
+const section = document.getElementById("caminhoPrincipal");
+const titulo = document.createElement("h1");
+titulo.innerText = "Introdução";
+const atividadeButton = document.createElement("button");
+atividadeButton.innerText = "1";
+const atividadeButton2 = document.createElement("button");
+atividadeButton2.innerText = "2";
+section.append(titulo, atividadeButton, atividadeButton2);
